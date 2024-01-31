8a2f897bc54c3b3f150c6445545050f9a1f3287f
william <1502318069@qq.com>
2024-01-31 11:32:20 +0800

    feature: VueLazyload

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,18 @@ import "@/styles/index.scss"
 import { i18n } from "./locales"
 import { createPinia } from "pinia"
 import VueClickAway from "vue3-click-away"
+import VueLazyload from "vue-lazyload"
 
 const pinia = createPinia()
-const app = createApp(App).use(router).use(pinia).use(i18n).use(VueClickAway)
+const app = createApp(App)
+  .use(router)
+  .use(pinia)
+  .use(i18n)
+  .use(VueClickAway)
+  .use(VueLazyload, {
+    lazyComponent: true,
+    observer: true,
+    loading: require("@/assets/default-cover.jpg"),
+    error: require("@/assets/default-cover.jpg"),
+  })
 app.mount("#app")
